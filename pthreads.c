9a38e7c2cd18e7d289b4892722acfa89645d19af
Kelsey-Bedard <kelsey.p.bedard@ndsu.edu>
2017-04-25 13:25:52 -0500

    Create pthreads.c

---
+++ pthreads.c
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdbool.h>
+#define NUM_THREADS     5
+
+bool solve(int row, int col, int* puzzle, int counter, int startValue)
+ {
+   return true;
+ }
+
+  bool valueAllowedCheck (int row, int col, int value, int* puzzle) {
+   return true;
+
+ }
+ void *run (void *threadid, int* puzzle)
+ {
+
+ }
+
+
+
+ int main (int argc, char*argv[]) {
+
+ printf("hello\n");
+
+
+ }
