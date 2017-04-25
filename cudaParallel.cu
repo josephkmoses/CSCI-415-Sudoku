82c8516067a1db0df49b1ec1c0371b8409b81e52
josephkmoses <joseph.k.moses6@gmail.com>
2017-04-25 13:28:28 -0500

    I moved the functions outside the kernel

--- cudaParallel.cu
+++ cudaParallel.cu
@@ -12,7 +12,80 @@
 
 using namespace std;
 
+__device__ bool solve(int row, int col, int puzzle, int counter, int startValue, bool* finished)
+{		
+	if(counter == 81) //every cell has been visted
+	{
+			return true;
+	}
+	if(finished)
+	{
+			return true;
+	}
 
+	//loop of column and rows
+	if(++col == 9)
+	{
+			col = 0;
+			if(++row == 9)
+			{
+					row = 0;
+			}
+	}
+
+	//skip cells that are already filled
+	if(puzzle[row * 9 + col] != 0)
+	{
+			return solve(row, col, puzzle, counter+1, startValue);
+	}
+
+	//if the cell is empty
+	for(int val = 1; val <= 9; val++)
+	{
+			//loop through the values being checked recursively
+			if(++startValue == 10)
+			{
+					startValue = 1;
+			}
+
+			//check if the value is allowed
+			if(valueAllowedCheck(row, col, startValue, puzzle))
+			{
+					puzzle[row * 9 + col] = startValue; //record if it is
+
+					if(solve(row, col, puzzle, counter+1, startValue)) //solve the next cell
+					{
+							return true;
+					}
+			}
+	}
+	puzzle[row * 9 + col] = 0; //set to zero if backtracking
+	return false;
+}
+
+__device__ bool valueAllowedCheck(int row, int col, int value, int puzzle)
+{
+		int i; //loop vairable
+
+		for(i = 0; i < 9; i++)
+		{
+				if(puzzle[row * 9 + i] == value) //check cells in the row
+				{
+						return false;
+				}
+				else if(puzzle[col + i * 9] == value) //check cells in the column
+				{
+						return false;
+				}
+				else if(puzzle[(row/3*3+i%3) * 9 + (col/3*3+i/3) ] == value) //check the subsquare
+				{
+						return false;
+				}
+		}
+
+		return true; //the value works
+}
+	
 __global__ void parallelSudoku(int* puzzle, bool* finished, char* result)
 {
 	int i = threadIdx.x;	//the thread id
@@ -25,81 +98,6 @@ __global__ void parallelSudoku(int* puzzle, bool* finished, char* result)
 	{
 		puzzleArray[i] = puzzle[i];
 	}
-
-	__device__ bool solve(int row, int col, int puzzle, int counter, int startValue, bool* finished)
-	{		
-        if(counter == 81) //every cell has been visted
-        {
-                return true;
-        }
-        if(finished)
-        {
-                return true;
-        }
-
-        //loop of column and rows
-        if(++col == 9)
-        {
-                col = 0;
-                if(++row == 9)
-                {
-                        row = 0;
-                }
-        }
-
-        //skip cells that are already filled
-        if(puzzle[row * 9 + col] != 0)
-        {
-                return solve(row, col, puzzle, counter+1, startValue);
-        }
-
-        //if the cell is empty
-        for(int val = 1; val <= 9; val++)
-        {
-                //loop through the values being checked recursively
-                if(++startValue == 10)
-                {
-                        startValue = 1;
-                }
-
-                //check if the value is allowed
-                if(valueAllowedCheck(row, col, startValue, puzzle))
-                {
-                        puzzle[row * 9 + col] = startValue; //record if it is
-
-                        if(solve(row, col, puzzle, counter+1, startValue)) //solve the next cell
-                        {
-                                return true;
-                        }
-                }
-        }
-        puzzle[row * 9 + col] = 0; //set to zero if backtracking
-        return false;
-	}
-	
-	__device__ bool valueAllowedCheck(int row, int col, int value, int puzzle)
-	{
-			int i; //loop vairable
-
-			for(i = 0; i < 9; i++)
-			{
-					if(puzzle[row * 9 + i] == value) //check cells in the row
-					{
-							return false;
-					}
-					else if(puzzle[col + i * 9] == value) //check cells in the column
-					{
-							return false;
-					}
-					else if(puzzle[(row/3*3+i%3) * 9 + (col/3*3+i/3) ] == value) //check the subsquare
-					{
-							return false;
-					}
-			}
-
-			return true; //the value works
-	}
-	
 
 	if(solve(i,j,puzzleArray,0,startVal, finished)) 
 	{
