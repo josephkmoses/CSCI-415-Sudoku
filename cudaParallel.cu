5a7cc0a7e688bb6e205ac023d0e951d5bdb3cd24
josephkmoses <joseph.k.moses6@gmail.com>
2017-04-30 17:44:20 -0500

    nevermind...

--- cudaParallel.cu
+++ cudaParallel.cu
@@ -107,7 +107,6 @@ __global__ void parallelSudoku(int* puzzle, bool* finished)
 	{
 		if(!finished)//none of the threads have finished the puzzle
 		{
-			cout << "Thread " << blockIdx.x * blockDim.x + threadIdx.x << " solved the puzzle.\n";
 			for(int i =0; i < 81; i++)
 			{
 				puzzle[i] = puzzleArray[i];
