0faaf11474bf2b55e1586f21d2d66fa89b315141
josephkmoses <joseph.k.moses6@gmail.com>
2017-05-01 14:20:22 -0500

    Trying to increase the stack size again

--- cudaParallel.cu
+++ cudaParallel.cu
@@ -313,7 +313,7 @@ int main() {
 	cudaMemcpy(d_result, h_result, sizeof(int), cudaMemcpyHostToDevice);
 	checkErrors("cudaMemcpy3");
 	
-	size_t stackSize = 512000;
+	size_t stackSize = 5120000;
 	cudaDeviceSetLimit(cudaLimitStackSize, stackSize);
 	
 	dim3 threadsPerBlock(9,9);
