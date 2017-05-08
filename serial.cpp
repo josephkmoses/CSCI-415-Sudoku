38430aceb679ec5595b20563ec247220a5ed6a84
Kelsey-Bedard <kelsey.p.bedard@ndsu.edu>
2017-05-08 13:31:08 -0500

    Update serial.cpp
    Add timers

--- serial.cpp
+++ serial.cpp
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <time.h>
 #include <iostream>
+#include <sys/time.h>
 using namespace std;
 
 static bool finished;
@@ -14,6 +15,23 @@ bool solve(int row, int col, int* puzzle, int counter, int startValue);
 bool valueAllowedCheck(int row, int col, int value, int* puzzle);
 void run(int* puzzle);
 
+
+long long start_timer() {
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+
+// Prints the time elapsed since the specified time
+long long stop_timer(long long start_time, char* name) {
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        long long end_time = tv.tv_sec * 1000000 + tv.tv_usec;
+        printf("%s",name);
+        printf("%f seconds\n",((float)(end_time - start_time)) / (1000 * 1000));
+        return end_time - start_time;
+}
 bool solve(int row, int col, int* puzzle, int counter, int startValue)
 {
         if(counter == 81) //every cell has been visted
@@ -215,6 +233,8 @@ int main() {
     evilPuzzle[i] = inputEvil[i];
     unsolveable[i] = inputUnsolve[i];
     }
+        long long start = start_timer();
          run(easyPuzzle);
          printPuzzle(easyPuzzle);
+        long long stop = stop_timer(start, "\nTotal Time: ");
 }
